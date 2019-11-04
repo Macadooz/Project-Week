e34fb3d51d6a217b444a3b8cfabdd8c78df989f0
Jackson Bremen <jacksonbremen@Jacksons-Mac-5.local>
2019-11-04 09:43:09 -0600

    Changed some database stuff

--- Database.java
+++ Database.java
@@ -1,34 +1,122 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.DriverManager;  
+import java.sql.Connection;  
+import java.sql.ResultSet;  
+import java.sql.SQLException;  
+import java.sql.PreparedStatement;  
 
 class Database {
-  private Connection db;
+    private String url;
 
-  public Database(String url) {
-    //some setup goes here
-    db = connect(url);
-  }
+    public Database(String url) {
+        //some setup goes here
+        this.url = url;
+    }
 
-  public getProjectPreference(Person p, int preference) {
-    //reference the DB, return the project ID or some shit
-    String sql = "SELECT projectID FROM data WHERE projectPreference = preference";
+    /**
+        public int getPreference(int studentId, int preference)
 
-  }
+        takes in a student ID and Preference number
 
-  public boolean getGender(Person p) {
+        Precondition:
+            studentId is valid
+    */
+    public int getPreference(int studentId, int preference) {
+        //reference the DB, return whatever
+        String sql = "SELECT ProjId FROM data WHERE StudentID = (?) AND PrefNum = (?)";
+
+        try {
+            Connection conn = this.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setDouble(1, studentId);
+            stmt.setDouble(2, preference);
+
+            ResultSet rs = stmt.executeQuery(sql)
+            return rs.getInt(0);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+        public String getGender(int studentId,)
+
+        takes in a student ID and returns the gender
+
+        Precondition:
+            studentId is valid
+    */
+    public String getGender(int studentId) {
     //reference the DB, return the gender
     //NOTE: seriously
-  }
+    }
 
-  public int[] getPrevYears(Person p) {
-    //returns a list of previous three years
-  }
+    /**
+        public int[] getPrevYears(int studentId) 
 
-  private Connection connect(String url) {
+        takes in a student ID, returns choices of previous 3 years
+
+        Precondition:
+            studentId is valid
+    */
+    public int[] getPrevYears(int studentId) {
+        //returns a list of previous three years
+    }
+
+    /**
+        public int[] getMinStudents(int projectId) 
+
+        takes in a project ID, returns minimum number of students for the project 
+
+        Precondition:  
+            projectId is valud
+    */
+    public int getMinStudents(int projID) {
+        String sql = "SELECT MinStudents FROM ProjStats WHERE ProjID = (?)";
+
+        try {
+            Connection conn = this.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setDouble(1, projID);
+
+            ResultSet rs = stmt.executeQuery(sql)
+            return rs.getInt(0);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    
+    }
+
+    /**
+        public int[] getMaxStudents(int projectId) 
+
+        takes in a project ID, returns maximum number of students for the project 
+
+        Precondition:  
+            projectId is valud
+    */
+    public int getMaxStudents(int projID) {
+        String sql = "SELECT MaxStudents FROM ProjStats WHERE ProjID = (?)";
+
+        try {
+            Connection conn = this.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setDouble(1, projID);
+
+            ResultSet rs = stmt.executeQuery(sql)
+            return rs.getInt(0);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private Connection connect() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(this.url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
