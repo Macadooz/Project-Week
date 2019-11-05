6d0b7d41276f5a077b223785d0d88a946c73617e
Jackson Bremen <jacksonbremen@Jacksons-Mac-5.local>
2019-11-04 18:43:23 -0600

    Added Bash file (makeAndRun.sh) to compile and run project including jar Updated Database.java further; all methods appear to be working with the exception of getPrevYears

--- Database.java
+++ Database.java
@@ -1,17 +1,37 @@
 //Changelog: Jackson
 
-import java.sql.DriverManager;  
-import java.sql.Connection;  
-import java.sql.ResultSet;  
-import java.sql.SQLException;  
-import java.sql.PreparedStatement;  
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
-class Database {
-    private String url;
+public class Database {
+    private final String url;
 
     public Database(String url) {
         //some setup goes here
         this.url = url;
+    }
+
+    //for testing 
+    public void printAll(){
+      String sql = "SELECT ? FROM StudentPrevAvgs WHERE StudentID = ?";
+
+      try {
+          Connection conn = this.connect();
+          PreparedStatement stmt = conn.prepareStatement(sql);
+
+          String test = "Rank16";
+          stmt.setString(1, test);
+          stmt.setString(2, "9");
+
+          ResultSet rs = stmt.executeQuery();
+          System.out.println(rs.getInt(1));
+
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
     }
 
     /**
@@ -24,7 +44,7 @@ class Database {
     */
     public int getPreference(int studentId, int preference) {
         //reference the DB, return whatever
-        String sql = "SELECT ProjId FROM data WHERE StudentID = (?) AND PrefNum = (?)";
+        String sql = "SELECT ProjId FROM Raw WHERE StudentID = (?) AND PrefNum = (?)";
 
         try {
             Connection conn = this.connect();
@@ -32,12 +52,13 @@ class Database {
             stmt.setDouble(1, studentId);
             stmt.setDouble(2, preference);
 
-            ResultSet rs = stmt.executeQuery(sql)
-            return rs.getInt(0);
+            ResultSet rs = stmt.executeQuery();
+            return rs.getInt(1);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return -1;
     }
 
     /**
@@ -49,12 +70,27 @@ class Database {
             studentId is valid
     */
     public String getGender(int studentId) {
-    //reference the DB, return the gender
-    //NOTE: seriously
+        //reference the DB, return the gender
+        //NOTE: might want to make this more PC in the future
+        String sql = "SELECT Gender FROM Raw WHERE StudentID = (?)";
+
+        try {
+            Connection conn = this.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setDouble(1, studentId);
+
+            ResultSet rs = stmt.executeQuery();
+            return rs.getString(1);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "-1";
+
     }
 
     /**
-        public int[] getPrevYears(int studentId) 
+        public int[] getPrevYears(int studentId)
 
         takes in a student ID, returns choices of previous 3 years
 
@@ -63,14 +99,35 @@ class Database {
     */
     public int[] getPrevYears(int studentId) {
         //returns a list of previous three years
+        int prevYears[] = new int[3];
+        String headers[] = new String[]{"Rank18","Rank17","Rank16"};
+        String sql;
+
+        sql = "SELECT (?) FROM StudentPrevAvgs WHERE StudentID = (?)";
+
+        try {
+            Connection conn = this.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, "Rank18");
+            stmt.setDouble(2, 1);
+
+            ResultSet rs = stmt.executeQuery();
+            System.out.println(rs.getInt(1));
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return prevYears;
     }
 
     /**
-        public int[] getMinStudents(int projectId) 
+        public int[] getMinStudents(int projectId)
 
-        takes in a project ID, returns minimum number of students for the project 
+        takes in a project ID, returns minimum number of students for the project
 
-        Precondition:  
+        Precondition:
             projectId is valud
     */
     public int getMinStudents(int projID) {
@@ -81,21 +138,21 @@ class Database {
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setDouble(1, projID);
 
-            ResultSet rs = stmt.executeQuery(sql)
-            return rs.getInt(0);
+            ResultSet rs = stmt.executeQuery();
+            return rs.getInt(1);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-    
+        return -1;
     }
 
     /**
-        public int[] getMaxStudents(int projectId) 
+        public int[] getMaxStudents(int projectId)
 
-        takes in a project ID, returns maximum number of students for the project 
+        takes in a project ID, returns maximum number of students for the project
 
-        Precondition:  
+        Precondition:
             projectId is valud
     */
     public int getMaxStudents(int projID) {
@@ -106,13 +163,13 @@ class Database {
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setDouble(1, projID);
 
-            ResultSet rs = stmt.executeQuery(sql)
-            return rs.getInt(0);
+            ResultSet rs = stmt.executeQuery();
+            return rs.getInt(1);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
+        return -1;
     }
 
     private Connection connect() {
---
+++ makeAndRun.sh
@@ -0,0 +1,3 @@
+javac -cp sqlite-jdbc-3.27.2.1.jar:. *.java
+
+java -cp sqlite-jdbc-3.27.2.1.jar:. DBTest
