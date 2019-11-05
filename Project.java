da8c74861556994f107366f336ad028b96ced2f1
sydberg <57270057+sydberg@users.noreply.github.com>
2019-11-05 12:04:03 -0600

    Update Project.java

---
+++ Project.java
@@ -0,0 +1,21 @@
+public class Project{
+	int enrolledstudents
+	int maxstudents
+	int projectID
+	int numBoys
+	int numgirls
+
+	public Project(int projectID, int maxstudents){
+		this.projectID = projectID;
+		this.maxstudents = maxstudents;
+		
+	}
+
+	public Project(int projectID, int enrolledstudents, int numBoys){
+		this.projectID = projectID;
+		this.enrolledstudents = enrolledstudents;
+		this.numBoys = numBoys;
+    this.numGirls = numGirls;
+    
+	}
+}
